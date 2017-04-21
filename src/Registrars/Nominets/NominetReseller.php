0c42d93149a63a43dacfeda724649d2d467899e9
Ninthz <nuttasak.t@gmail.com>
2017-04-21 15:42:52 +0700

    create reference attribute in NominetReseller

--- src/Registrars/Nominets/NominetReseller.php
+++ src/Registrars/Nominets/NominetReseller.php
@@ -11,92 +11,118 @@ use LaravelEPP\Registrars\Nominets\Nominet;
 class NominetReseller extends Nominet
 {
 
-  function __construct()
-  {
-    parent::__construct();
-  }
+    /**
+     * Reference like a reseller id
+     */
+    private $reference;
 
-  function __destruct()
-  {
-    $this->logout();
-    parent::__destruct();
-  }
-
-  function create($parameters)
-  {
-    if ($this->login(true)) {
-      $xml = file_get_contents($this->getDataXMLPath('create-reseller'));
-      $mappers = [
-        '{reference}'    => $parameters['reference'] ?? '',
-        '{trading_name}' => $parameters['trading_name'] ?? '',
-        '{url}' => $parameters['url'] ?? '',
-        '{email}' => $parameters['email'] ?? '',
-        '{telephone}' => $parameters['telephone'] ?? '',
-      ];
-      $xml = $this->mapParameters($xml, $mappers);
-      return  $this->epp_client->sendRequest($xml);
+    public function __construct($reference = '')
+    {
+        parent::__construct();
+        $this->reference = $reference;
     }
-  }
 
-  function delete($parameters)
-  {
-    if ($this->login(true)) {
-      $xml = file_get_contents($this->getDataXMLPath('delete-reseller'));
-      $mappers = [
-        '{reference}' => $parameters['reference'] ?? ''
-      ];
-      $xml = $this->mapParameters($xml, $mappers);
-      return  $this->epp_client->sendRequest($xml);
+    public function __destruct()
+    {
+        $this->logout();
+        parent::__destruct();
     }
-  }
 
-  function info($parameters)
-  {
-    if ($this->login(true)) {
-      $xml = file_get_contents($this->getDataXMLPath('info-reseller'));
-      $mappers = [
-        '{reference}' => $parameters['reference']
-      ];
-      $xml = $this->mapParameters($xml, $mappers);
-      return  $this->epp_client->sendRequest($xml);
+    /**
+     * Get the value of reference
+     *
+     * @return string
+     */
+    public function getReference()
+    {
+        return $this->reference;
     }
-  }
 
-  function list()
-  {
-    if ($this->login(true)) {
-      $xml = file_get_contents($this->getDataXMLPath('list-reseller'));
-      return $this->epp_client->sendRequest($xml);
+    /**
+     * Set the value of reference
+     *
+     * @param string reference
+     *
+     * @return self
+     */
+    public function setReference($reference)
+    {
+        $this->reference = $reference;
+
+        return $this;
     }
-  }
 
-  function update($parameters)
-  {
-    if ($this->login(true)) {
-      $xml = file_get_contents($this->getDataXMLPath('update-reseller'));
-      $mappers = [
-        '{reference}'    => $parameters['reference'] ?? '',
-        '{trading_name}' => $parameters['trading_name'] ?? '',
-        '{url}' => $parameters['url'] ?? '',
-        '{email}' => $parameters['email'] ?? '',
-        '{telephone}' => $parameters['telephone'] ?? '',
-      ];
-      $xml = $this->mapParameters($xml, $mappers);
-      $response = $this->epp_client->sendRequest($xml);
+    public function makeMapper($parameters = [])
+    {
+      return array_merge($parameters, ['{reference}' => $this->reference]);
+    }
 
-      if(!$response['status'])
-      {
-        if($response['message'] != 'Error: Parameter value range error')
-        {
-          return $response;
+    function create($tradingName = '', $url = '', $email = '', $telephone = '')
+    {
+        if ($this->login(true)) {
+          $xml = file_get_contents($this->getDataXMLPath('create-reseller'));
+
+          $mappers = $this->makeMapper([
+              '{trading_name}' => $tradingName,
+              '{url}' => $url,
+              '{email}' => $email,
+              '{telephone}' => $telephone,
+          ]);
+
+          $xml = $this->mapParameters($xml, $mappers);
+          return  $this->epp_client->sendRequest($xml);
         }
-
-        $response['status'] = true;
-        $response['message'] = 'Update success';
-        return $response;
-      }
-
-      return $response;
     }
-  }
+
+    function delete()
+    {
+        if ($this->login(true)) {
+          $xml = file_get_contents($this->getDataXMLPath('delete-reseller'));
+
+          $mappers = $this->makeMapper();
+
+          $xml = $this->mapParameters($xml, $mappers);
+          return  $this->epp_client->sendRequest($xml);
+        }
+    }
+
+    function info()
+    {
+        if ($this->login(true)) {
+          $xml = file_get_contents($this->getDataXMLPath('info-reseller'));
+
+          $mappers = $this->makeMapper();
+
+          $xml = $this->mapParameters($xml, $mappers);
+          return  $this->epp_client->sendRequest($xml);
+        }
+    }
+
+    function list()
+    {
+        if ($this->login(true)) {
+          $xml = file_get_contents($this->getDataXMLPath('list-reseller'));
+          return $this->epp_client->sendRequest($xml);
+        }
+    }
+
+    function update($tradingName = '', $url = '', $email = '', $telephone = '')
+    {
+        if ($this->login(true)) {
+          $xml = file_get_contents($this->getDataXMLPath('update-reseller'));
+
+          $mappers = $this->makeMapper([
+            '{trading_name}' => $tradingName,
+            '{url}' =>  $url = '',
+            '{email}' => $email,
+            '{telephone}' => $telephone,
+          ]);
+
+          $xml = $this->mapParameters($xml, $mappers);
+          return $this->epp_client->sendRequest($xml);
+        }
+    }
+
+
+
 }
