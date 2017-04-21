473f92038f729feb4d5bc122fc9f26716e2c4d16
Ninthz <nuttasak.t@gmail.com>
2017-04-21 13:59:29 +0700

    Refactor nominee contact

--- src/EPP/Tools/XMLDom.php
+++ src/EPP/Tools/XMLDom.php
@@ -12,7 +12,7 @@ class XMLDom extends \DOMDocument {
 	//public $ns_contact_ext = 'http://www.nominet.org.uk/epp/xml/nom-contact-2.0';
 	public $ns_host = 'urn:ietf:params:xml:ns:host-1.0';
 
-	public function FromMixed($mixed, DOMElement $domElement = null) {
+	public function fromMixed($mixed, DOMElement $domElement = null) {
 		$domElement = is_null($domElement) ? $this : $domElement;
 
 		if(is_array($mixed)) {
@@ -34,23 +34,23 @@ class XMLDom extends \DOMDocument {
 					foreach($mixedElement['attr'] as $key => $value)
 						$node->setAttribute($key, $value);
 
-					$this->FromMixed($mixedElement['body'], $node);
+					$this->fromMixed($mixedElement['body'], $node);
 				}
 				else
-					$this->FromMixed($mixedElement, $node);
+					$this->fromMixed($mixedElement, $node);
 			}
 		}
 		else
 			$domElement->appendChild($this->createTextNode($mixed));
-	} // FromMixed()
+	}
 
 	public function getMessage() {
 		return $this->getElementsByTagName("msg")->item(0)->nodeValue;
-	} // GetMessage()
+	}
 
 	public function getCode() {
 		return $this->getElementsByTagName("result")->item(0)->getAttribute('code');
-	} // GetCode()
+	}
 
 	public function getReason() {
 		return $this->getElementsByTagName("reason")->item(0)->nodeValue;
@@ -58,18 +58,18 @@ class XMLDom extends \DOMDocument {
 
 	public function GetID() {
 		return $this->getElementsByTagName("clTRID")->item(0)->nodeValue;
-	} // GetID()
+	}
 
-	public function GetDataItem($ns, $name, $dom_element = null, $nodeIndex = 0) {
+	public function getDataItem($ns, $name, $dom_element = null, $nodeIndex = 0) {
 		$dom_element = is_null($dom_element) ? $this : $dom_element;
 
 		if($dom_element->getElementsByTagNameNS($ns, $name)->length > $nodeIndex)
 			return $dom_element->getElementsByTagNameNS($ns, $name)->item($nodeIndex)->nodeValue;
 		else
 			return '';
-	} // GetDataItem()
+	}
 
-	public function GetDataAttribute($ns, $name, $attribute, $dom_element = null) {
+	public function getDataAttribute($ns, $name, $attribute, $dom_element = null) {
 		$dom_element = is_null($dom_element) ? $this : $dom_element;
 
 		if($dom_element->getElementsByTagNameNS($ns, $name)->length > 0) {
@@ -80,67 +80,67 @@ class XMLDom extends \DOMDocument {
 		}
 		else
 			return '';
-	} // GetDataAttribute()
+	}
 
 	public function getContactInfo() {
 		$contact_info               = [];
-		$contact_info['id']					= $this->GetDataItem($this->ns_contact, 'id');
-		$contact_info['roid']				= $this->GetDataItem($this->ns_contact, 'roid');
+		$contact_info['id']					= $this->getDataItem($this->ns_contact, 'id');
+		$contact_info['roid']				= $this->getDataItem($this->ns_contact, 'roid');
 
 		foreach($this->getElementsByTagNameNS($this->ns_contact, 'postalInfo') as $postalInfo) {
-			$contact_info['name'] 		= $this->GetDataItem($this->ns_contact, 'name', $postalInfo);
-			$contact_info['org'] 			= $this->GetDataItem($this->ns_contact, 'org', $postalInfo);
+			$contact_info['name'] 		= $this->getDataItem($this->ns_contact, 'name', $postalInfo);
+			$contact_info['org'] 			= $this->getDataItem($this->ns_contact, 'org', $postalInfo);
 
 			foreach($this->getElementsByTagNameNS($this->ns_contact, 'addr') as $address) {
-				$contact_info['street'] 	= $this->GetDataItem($this->ns_contact, 'street', $address);
-				$contact_info['street2'] 	= $this->GetDataItem($this->ns_contact, 'street', $address, 1);
-				$contact_info['street3'] 	= $this->GetDataItem($this->ns_contact, 'street', $address, 2);
-				$contact_info['city'] 		= $this->GetDataItem($this->ns_contact, 'city', $address);
-				$contact_info['sp'] 			= $this->GetDataItem($this->ns_contact, 'sp', $address);
-				$contact_info['pc'] 			= $this->GetDataItem($this->ns_contact, 'pc', $address);
-				$contact_info['cc']	 			= $this->GetDataItem($this->ns_contact, 'cc', $address);
+				$contact_info['street'] 	= $this->getDataItem($this->ns_contact, 'street', $address);
+				$contact_info['street2'] 	= $this->getDataItem($this->ns_contact, 'street', $address, 1);
+				$contact_info['street3'] 	= $this->getDataItem($this->ns_contact, 'street', $address, 2);
+				$contact_info['city'] 		= $this->getDataItem($this->ns_contact, 'city', $address);
+				$contact_info['sp'] 			= $this->getDataItem($this->ns_contact, 'sp', $address);
+				$contact_info['pc'] 			= $this->getDataItem($this->ns_contact, 'pc', $address);
+				$contact_info['cc']	 			= $this->getDataItem($this->ns_contact, 'cc', $address);
 			}
 		}
 
-		$contact_info['voice'] 			= $this->GetDataItem($this->ns_contact, 'voice');
-		$contact_info['email'] 			= $this->GetDataItem($this->ns_contact, 'email');
-		$contact_info['disclose'] 	= $this->GetDataAttribute($this->ns_contact, 'disclose', 'flag');
+		$contact_info['voice'] 			= $this->getDataItem($this->ns_contact, 'voice');
+		$contact_info['email'] 			= $this->getDataItem($this->ns_contact, 'email');
+		$contact_info['disclose'] 	= $this->getDataAttribute($this->ns_contact, 'disclose', 'flag');
 
 		// Nominet extension values
-		$contact_info['type'] 			= $this->GetDataItem($this->ns_contact_ext,'type');
-		$contact_info['opt_out']		= $this->GetDataItem($this->ns_contact_ext, 'opt-out');
+		$contact_info['type'] 			= $this->getDataItem($this->ns_contact_ext,'type');
+		$contact_info['opt_out']		= $this->getDataItem($this->ns_contact_ext, 'opt-out');
 
 		return $contact_info;
 	}
 
 	public function getCheckContact() {
 		$contact_info = [];
-		$contact_info['id'] = $this->GetDataItem($this->ns_contact, 'id');
-		$contact_info['reason'] = $this->GetDataItem($this->ns_contact, 'reason');
+		$contact_info['id'] = $this->getDataItem($this->ns_contact, 'id');
+		$contact_info['reason'] = $this->getDataItem($this->ns_contact, 'reason');
 
 		return $contact_info;
 	}
 
 	public function getDomainInfo() {
 		$domain_info 								  = [];
-		$domain_info['roid'] 					= $this->GetDataItem($this->ns_domain, 'roid');
-		$domain_info['domain'] 				= $this->GetDataItem($this->ns_domain,'name');
-		$domain_info['reg_status'] 		= $this->GetDataItem($this->ns_domain_ext,'reg-status');
-		$domain_info['reseller'] 		= $this->GetDataItem($this->ns_domain_ext,'reseller');
-		$domain_info['registrant'] 		= $this->GetDataItem($this->ns_domain, 'registrant');
+		$domain_info['roid'] 					= $this->getDataItem($this->ns_domain, 'roid');
+		$domain_info['domain'] 				= $this->getDataItem($this->ns_domain,'name');
+		$domain_info['reg_status'] 		= $this->getDataItem($this->ns_domain_ext,'reg-status');
+		$domain_info['reseller'] 		= $this->getDataItem($this->ns_domain_ext,'reseller');
+		$domain_info['registrant'] 		= $this->getDataItem($this->ns_domain, 'registrant');
 
 		$domain_info['account'] 					  = [];
-		$domain_info['account']['id'] 			= $this->GetDataItem($this->ns_account,'roid');
-		$domain_info['account']['name'] 		= $this->GetDataItem($this->ns_account,'name');
-		$domain_info['account']['type'] 		= $this->GetDataItem($this->ns_account,'type');
-		$domain_info['account']['opt_out'] 	= $this->GetDataItem($this->ns_account,'opt-out');
+		$domain_info['account']['id'] 			= $this->getDataItem($this->ns_account,'roid');
+		$domain_info['account']['name'] 		= $this->getDataItem($this->ns_account,'name');
+		$domain_info['account']['type'] 		= $this->getDataItem($this->ns_account,'type');
+		$domain_info['account']['opt_out'] 	= $this->getDataItem($this->ns_account,'opt-out');
 
 		$address               = [];
-		$address['street']     = $this->GetDataItem($this->ns_account,'street');
-		$address['city']       = $this->GetDataItem($this->ns_account,'city');
-		$address['county']     = $this->GetDataItem($this->ns_account,'county');
-		$address['post_code']  = $this->GetDataItem($this->ns_account,'postcode');
-		$address['country']    = $this->GetDataItem($this->ns_account,'country');
+		$address['street']     = $this->getDataItem($this->ns_account,'street');
+		$address['city']       = $this->getDataItem($this->ns_account,'city');
+		$address['county']     = $this->getDataItem($this->ns_account,'county');
+		$address['post_code']  = $this->getDataItem($this->ns_account,'postcode');
+		$address['country']    = $this->getDataItem($this->ns_account,'country');
 
 		$domain_info['account']['address'] = $address;
 		$domain_info['account']['contact'] = [];
@@ -149,57 +149,57 @@ class XMLDom extends \DOMDocument {
 
 		foreach($this->getElementsByTagNameNS($this->ns_account,'contact') as $contact) {
 			$domain_info['account']['contact'][$i] = [];
-			$domain_info['account']['contact'][$i]['id'] = $this->GetDataItem($this->ns_contact,'roid', $contact);
-			$domain_info['account']['contact'][$i]['name'] = $this->GetDataItem($this->ns_contact,'name', $contact);
-			$domain_info['account']['contact'][$i]['phone'] = $this->GetDataItem($this->ns_contact,'phone', $contact);
-			$domain_info['account']['contact'][$i]['email'] = $this->GetDataItem($this->ns_contact,'email', $contact);
-			$domain_info['account']['contact'][$i]['tag'] = $this->GetDataItem($this->ns_contact,'clID', $contact);
-			$domain_info['account']['contact'][$i]['updated_id'] = $this->GetDataItem($this->ns_contact,'upID', $contact);
-			$domain_info['account']['contact'][$i]['updated_date'] = $this->GetDataItem($this->ns_contact,'upDate', $contact);
+			$domain_info['account']['contact'][$i]['id'] = $this->getDataItem($this->ns_contact,'roid', $contact);
+			$domain_info['account']['contact'][$i]['name'] = $this->getDataItem($this->ns_contact,'name', $contact);
+			$domain_info['account']['contact'][$i]['phone'] = $this->getDataItem($this->ns_contact,'phone', $contact);
+			$domain_info['account']['contact'][$i]['email'] = $this->getDataItem($this->ns_contact,'email', $contact);
+			$domain_info['account']['contact'][$i]['tag'] = $this->getDataItem($this->ns_contact,'clID', $contact);
+			$domain_info['account']['contact'][$i]['updated_id'] = $this->getDataItem($this->ns_contact,'upID', $contact);
+			$domain_info['account']['contact'][$i]['updated_date'] = $this->getDataItem($this->ns_contact,'upDate', $contact);
 			$i++;
 		}
 
-		$domain_info['account']['tag'] = $this->GetDataItem($this->ns_account,'clID');
-		$domain_info['account']['creation_id'] = $this->GetDataItem($this->ns_account,'crID');
-		$domain_info['account']['creation_date'] = $this->GetDataItem($this->ns_account,'crDate');
-		$domain_info['account']['updated_id'] = $this->GetDataItem($this->ns_account,'upID');
-		$domain_info['account']['updated_date'] = $this->GetDataItem($this->ns_account,'upDate');
+		$domain_info['account']['tag'] = $this->getDataItem($this->ns_account,'clID');
+		$domain_info['account']['creation_id'] = $this->getDataItem($this->ns_account,'crID');
+		$domain_info['account']['creation_date'] = $this->getDataItem($this->ns_account,'crDate');
+		$domain_info['account']['updated_id'] = $this->getDataItem($this->ns_account,'upID');
+		$domain_info['account']['updated_date'] = $this->getDataItem($this->ns_account,'upDate');
 
 		$ns = [];
 		$i = 0;
 
 		foreach($this->getElementsByTagNameNS($this->ns_domain,'host') as $host) {
 			$ns['host'][$i] = [];
-			$ns['host'][$i]['host_name'] = $this->GetDataItem($this->ns_domain,'hostName', $host);
-			$ns['host'][$i]['host_address'] = $this->GetDataItem($this->ns_domain,'hostAddr', $host);
-			$ns['host'][$i]['ip_version'] = $this->GetDataAttribute($this->ns_domain, 'hostAddr', 'ip', $host);
+			$ns['host'][$i]['host_name'] = $this->getDataItem($this->ns_domain,'hostName', $host);
+			$ns['host'][$i]['host_address'] = $this->getDataItem($this->ns_domain,'hostAddr', $host);
+			$ns['host'][$i]['ip_version'] = $this->getDataAttribute($this->ns_domain, 'hostAddr', 'ip', $host);
 			$i++;
 		}
 
 		$domain_info['ns'] = $ns;
-		$domain_info['first_bill'] = $this->GetDataItem($this->ns_domain,'first-bill');
-		$domain_info['recur_bill'] = $this->GetDataItem($this->ns_domain,'recur-bill');
-		$domain_info['tag'] = $this->GetDataItem($this->ns_domain,'clID');
-		$domain_info['creation_id'] = $this->GetDataItem($this->ns_domain,'crID');
-		$domain_info['creation_date'] = $this->GetDataItem($this->ns_domain,'crDate');
-		$domain_info['updated_id'] = $this->GetDataItem($this->ns_domain,'upID');
-		$domain_info['updated_date'] = $this->GetDataItem($this->ns_domain,'upDate');
-		$domain_info['expiry_date'] = $this->GetDataItem($this->ns_domain,'exDate');
+		$domain_info['first_bill'] = $this->getDataItem($this->ns_domain,'first-bill');
+		$domain_info['recur_bill'] = $this->getDataItem($this->ns_domain,'recur-bill');
+		$domain_info['tag'] = $this->getDataItem($this->ns_domain,'clID');
+		$domain_info['creation_id'] = $this->getDataItem($this->ns_domain,'crID');
+		$domain_info['creation_date'] = $this->getDataItem($this->ns_domain,'crDate');
+		$domain_info['updated_id'] = $this->getDataItem($this->ns_domain,'upID');
+		$domain_info['updated_date'] = $this->getDataItem($this->ns_domain,'upDate');
+		$domain_info['expiry_date'] = $this->getDataItem($this->ns_domain,'exDate');
 		return $domain_info;
 	}
 
 	public function getCheckDomain() {
 		$domain_info = [];
-		$domain_info['name'] = $this->GetDataItem($this->ns_domain, 'name');
-		$domain_info['reason'] = $this->GetDataItem($this->ns_domain, 'reason');
+		$domain_info['name'] = $this->getDataItem($this->ns_domain, 'name');
+		$domain_info['reason'] = $this->getDataItem($this->ns_domain, 'reason');
 
 		return $domain_info;
 	}
 
 	public function getCheckHost() {
 		$host_info = [];
-		$host_info['name'] = $this->GetDataItem($this->ns_host, 'name');
-		$host_info['reason'] = $this->GetDataItem($this->ns_host, 'reason');
+		$host_info['name'] = $this->getDataItem($this->ns_host, 'name');
+		$host_info['reason'] = $this->getDataItem($this->ns_host, 'reason');
 
 		return $host_info;
 	}
--- src/Registrars/Nominets/NominetContact.php
+++ src/Registrars/Nominets/NominetContact.php
@@ -10,107 +10,140 @@ use LaravelEPP\Registrars\Nominets\Nominet;
  */
 class NominetContact extends Nominet
 {
+    private $contactId;
 
-  const CONTACT_TYPE_INDIVIDUAL = 'IND';
+    const CONTACT_TYPE_INDIVIDUAL = 'IND';
 
-  const CONTACT_TYPE_FOREIGN_INDIVIDUAL = 'FIND';
+    const CONTACT_TYPE_FOREIGN_INDIVIDUAL = 'FIND';
 
-  const CONTACT_TYPE_LIMITED = 'LTD';
+    const CONTACT_TYPE_LIMITED = 'LTD';
 
-  const CONTACT_TYPE_NON_UK_COPERATION = 'FCORP';
+    const CONTACT_TYPE_NON_UK_COPERATION = 'FCORP';
 
-  public function __construct()
-  {
-    parent::__construct();
-  }
-
-  public function __destruct()
-  {
-    $this->logout();
-    parent::__destruct();
-  }
-
-  public function optOut($parameters)
-  {
-    if ($this->login()) {
-      $xml = file_get_contents($this->getDataXMLPath('optout-contact'));
-      $mappers = [
-        '{contact_id}'    => $parameters['contact_id'] ?? '',
-        '{opt_out}' => ($parameters['opt_out'] == true ? 'Y' : 'N'),
-      ];
-      $xml = $this->mapParameters($xml, $mappers);
-      return  $this->epp_client->sendRequest($xml);
+    public function __construct($contactId = '')
+    {
+      parent::__construct();
+      $this->contactId = $contactId;
     }
-  }
 
-  public function privacy($parameters)
-  {
-    if ($this->login()) {
-      $xml = file_get_contents($this->getDataXMLPath('privacy-contact'));
-      $mappers = [
-        '{contact_id}'    => $parameters['contact_id'] ?? '',
-        '{privacy}' => ($parameters['privacy'] == true ? '0' : '1'),
-      ];
-      $xml = $this->mapParameters($xml, $mappers);
-      return  $this->epp_client->sendRequest($xml);
+    public function __destruct()
+    {
+      $this->logout();
+      parent::__destruct();
     }
-  }
 
-  public function info($parameters)
-  {
-    if ($this->login()) {
-      $xml = file_get_contents($this->getDataXMLPath('info-contact'));
-      $mappers = [
-        '{contact_id}'    => $parameters['contact_id'] ?? ''
-      ];
-      $xml = $this->mapParameters($xml, $mappers);
-      return  $this->epp_client->sendRequest($xml);
+    public function getContactId()
+    {
+        return $this->contactId;
     }
-  }
 
-  public function updateType($parameters)
-  {
-    if ($this->login()) {
-      $xml = file_get_contents($this->getDataXMLPath('update-contact-type'));
-      $mappers = [
-        '{contact_id}'    => $parameters['contact_id'] ?? '',
-        '{type}'          => $parameters['type'] ?? ''
-      ];
-      $xml = $this->mapParameters($xml, $mappers);
-      return  $this->epp_client->sendRequest($xml);
+    public function setContactId($contactId)
+    {
+        $this->contactId = $contactId;
+
+        return $this;
     }
-  }
 
-  public function updateTradingName($parameters)
-  {
-    if ($this->login()) {
-      $xml = file_get_contents($this->getDataXMLPath('update-contact-trading-name'));
-      $mappers = [
-        '{contact_id}'    => $parameters['contact_id'] ?? '',
-        '{trading_name}'   => $parameters['trading_name'] ?? ''
-      ];
-      $xml = $this->mapParameters($xml, $mappers);
-      return  $this->epp_client->sendRequest($xml);
+    public function makeMapper($parameters = [])
+    {
+      return array_merge($parameters, ['{contact_id}' => $contactId]);
     }
-  }
 
-  public function removeTradingName($contactId)
-  {
-    return $this->updateTradingName([
-      'contact_id' => $contactId,
-      'trading_name' => '',
-    ]);
-  }
+    public function optOut($parameters)
+    {
+      if ($this->login()) {
+        $xml = file_get_contents($this->getDataXMLPath('optout-contact'));
 
-  public function check(String $contactId)
-  {
-    if ($this->login()) {
-      $xml = file_get_contents($this->getDataXMLPath('check-contact'));
-      $mappers = [
-        '{contact_id}' => $contactId,
-      ];
-      $xml = $this->mapParameters($xml, $mappers);
-      return  $this->epp_client->sendRequest($xml);
+        $mappers = $this->makeMapper([
+          '{opt_out}' => ($parameters['opt_out'] == true ? 'Y' : 'N')
+        ]);
+
+        $xml = $this->mapParameters($xml, $mappers);
+        return  $this->epp_client->sendRequest($xml);
+      }
     }
-  }
+
+    public function privacy($parameters)
+    {
+      if ($this->login()) {
+        $xml = file_get_contents($this->getDataXMLPath('privacy-contact'));
+
+        $mappers = $this->makeMapper([
+          '{privacy}' => ($parameters['privacy'] == true ? '0' : '1')
+        ]);
+
+        $xml = $this->mapParameters($xml, $mappers);
+        return  $this->epp_client->sendRequest($xml);
+      }
+    }
+
+    public function info($parameters)
+    {
+      if ($this->login()) {
+        $xml = file_get_contents($this->getDataXMLPath('info-contact'));
+
+        $mappers = $this->makeMapper();
+
+        $xml = $this->mapParameters($xml, $mappers);
+        return  $this->epp_client->sendRequest($xml);
+      }
+    }
+
+    public function updateType($parameters)
+    {
+      if ($this->login()) {
+        $xml = file_get_contents($this->getDataXMLPath('update-contact-type'));
+
+        $mappers = $this->makeMapper([
+          '{type}'          => $parameters['type'] ?? ''
+        ]);
+
+        $xml = $this->mapParameters($xml, $mappers);
+        return  $this->epp_client->sendRequest($xml);
+      }
+    }
+
+    public function updateTradingName($parameters)
+    {
+      if ($this->login()) {
+        $xml = file_get_contents($this->getDataXMLPath('update-contact-trading-name'));
+
+        $mappers = $this->makeMapper([
+          '{trading_name}'          => $parameters['trading_name'] ?? ''
+        ]);
+
+        $xml = $this->mapParameters($xml, $mappers);
+        return  $this->epp_client->sendRequest($xml);
+      }
+    }
+
+    public function removeTradingName()
+    {
+      return $this->updateTradingName();
+    }
+
+    public function check(String $contactId)
+    {
+      if ($this->login()) {
+        $xml = file_get_contents($this->getDataXMLPath('check-contact'));
+
+        $mappers = $this->makeMapper();
+
+        $xml = $this->mapParameters($xml, $mappers);
+        return  $this->epp_client->sendRequest($xml);
+      }
+    }
+
+    public function validate()
+    {
+      if ($this->login()) {
+        $xml = file_get_contents($this->getDataXMLPath('validate-contact'));
+
+        $mappers = $this->makeMapper();
+
+        $xml = $this->mapParameters($xml, $mappers);
+        return  $this->epp_client->sendRequest($xml);
+      }
+    }
+
 }
