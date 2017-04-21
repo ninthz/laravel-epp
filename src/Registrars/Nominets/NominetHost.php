5f47188b6386c8020bf58c9318dce811c55c47c9
Keng <thanan_meepalang@siam.edu>
2017-04-21 11:44:37 +0700

    Add NominetHost

---
+++ src/Registrars/Nominets/NominetHost.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace LaravelEPP\Registrars\Nominets;
+
+use LaravelEPP\EPP\EppClient;
+use LaravelEPP\Registrars\Nominets\Nominet;
+
+/**
+ * Nominet Reseller class service
+ */
+class NominetHost extends Nominet
+{
+
+  public function __construct()
+  {
+    parent::__construct();
+  }
+
+  public function __destruct()
+  {
+    $this->logout();
+    parent::__destruct();
+  }
+
+  public function checkHost(String $hostName)
+  {
+    if ($this->login()) {
+      $xml = file_get_contents($this->getDataXMLPath('check-host'));
+      $mappers = [
+        '{host_name}' => $hostName,
+      ];
+      $xml = $this->mapParameters($xml, $mappers);
+      return  $this->epp_client->sendRequest($xml);
+    }
+  }
+}
