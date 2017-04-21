ad24cd82aab5597565e4508ded0ec9920970b5c5
Keng <thanan_meepalang@siam.edu>
2017-04-21 15:19:45 +0700

    Change how to pass contact id value

--- examples/check_contact.php
+++ examples/check_contact.php
@@ -8,12 +8,12 @@ $username = getenv('NOMINET_LIVE_USERNAME');
 $password = getenv('NOMINET_LIVE_PASSWORD');
 $host = 'epp.nominet.org.uk';
 
-$nc = new NominetContact();
+$contactId = 'NEO_47455933';
+$nc = new NominetContact($contactId);
 $nc->setHost($host);
 $nc->setUsername($username);
 $nc->setPassword($password);
 
-$contactId = 'NEO_47455933';
-$response = $nc->check($contactId)->toArray();
+$response = $nc->check()->toArray();
 
 var_dump($response['dom']->checkMapper());
